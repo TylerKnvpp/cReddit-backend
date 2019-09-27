27cb2e4479230355a6b34b16fe4d7d5b1c528cc0
Andrew Santos <asantos@alumni.nd.edu>
2019-09-27 13:16:00 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({
+    name: 'a',
+    username: 'a',
+    password: 'a',
+    avatar: nil,
+    credibility: 100
+})
+Post.create({
+    title: 'Here is a post',
+    content: 'Here is the content for the post',
+    image_url: nil,
+    user_id: 1,
+    upvotes: 100,
+    downvotes: 50
+})
+Comment.create({
+    content: "Comment content",
+    upvotes: 100,
+    downvotes: 31,
+    source: nil,
+    source_validated: nil,
+    source_disputed: nil,
+    is_fact?: false,
+    user_id: 1,
+    post_id: 1,
+})
